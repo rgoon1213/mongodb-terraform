2bbc2239236d03fd2eacd232819b0ffb9fe69900
GOON <raunakgoon@northwesternmutual.com>
2023-04-28 11:36:58 -0500

    linting

---
+++ mongodb_atlas_module.tf
@@ -0,0 +1,15 @@
+module "mongodb_atlas" {
+  # Source Module
+  source              = " "
+
+  # Required Modules
+  kms_key_id          =
+  tags                = 
+
+  # Module Variables
+  atlas_project_id    = 
+  cluster_name        = 
+  instance_size       = 
+  public_key          = 
+  private_key         = 
+}
